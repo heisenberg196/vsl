652f6c6d03f45c64b14fb6159050fe4ab984d0a1
Jatin Popli <jatinpopli196@gmail.com>
2021-10-13 22:08:55 +0530

    postgres

--- vsl/settings.py
+++ vsl/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z0_7vgsaknnub5-=93y&59k7xzeb_rn%)0pk_$004wae=umcwi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'vsl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vsldb',
+        'USER': 'djuser',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -121,3 +125,5 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
